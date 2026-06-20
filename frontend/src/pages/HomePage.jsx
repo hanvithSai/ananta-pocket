09725adf5515fdf90903430976e977f7014d6588
hanvithSai <hanvithsai.a@gmail.com>
2026-06-20 23:06:07 +0530

    No Products Found screen in HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Container, VStack, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+// import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
-  )
-}
+    <Container maxW="container.xl" py={12}>
+      <VStack spacing={8}>
+        <Text
+          fontSize={"30"}
+          fontWeight={"bold"}
+          textAlign={"center"}
+          bgGradient={"linear(to-r, cyan.400, blue.500)"}
+          bgClip={"text"}
+        >
+          Current Products
+        </Text>
 
-export default HomePage
+        <Text
+          fontSize="xl"
+          textAlign={"center"}
+          fontWeight="bold"
+          color="gray.500"
+        >
+          No Products Found. 
+          <br />
+          <Link
+            to="/create"
+            style={{ color: "blue.500", _hover: { textDecoration: "underline" } }}
+          >
+            <Text as="span" color="blue.500" fontWeight="bold">
+              Create Product
+            </Text>
+          </Link>
+        </Text>
+      </VStack>
+    </Container>
+  );
+};
+
+export default HomePage;
