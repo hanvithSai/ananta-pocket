2594b78d34cb4a0032c41f3a7d5f3eced9cadac9
hanvithSai <hanvithsai.a@gmail.com>
2026-06-22 18:29:59 +0530

    deployment config

--- backend/server.js
+++ backend/server.js
@@ -5,21 +5,28 @@ import express from "express"; // NEW convention using ES6 modules
 import dotenv from "dotenv"; // Import the dotenv package to load environment variables from a .env file
 import { connectDB } from "./config/db.js"; // Import the function to connect to the database
 import productRoutes from "./routes/product.route.js"; // Import the product routes
-
+import path from "path"; // Import the path module to work with file and directory paths
 
 dotenv.config(); // Load environment variables from the .env file
 
 const app = express();
 const PORT = process.env.PORT || 5000; // Set the port number from the environment variable or default to 5000
-
+const __dirname = path.resolve(); // Get the current directory path
 
 app.use(express.json()); // Middleware to parse JSON data from request bodies
+
+app.use("/api/products", productRoutes); // Use the product routes for any requests to /api/products
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist"))); // Serve static files from the React build directory
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")); // Serve the index.html file for any unknown routes (for React Router)
+  });
+}
 
 // app.get("/", (req, res) => {
 //   res.send("Server is ready");
 // });
-
-app.use("/api/products", productRoutes); // Use the product routes for any requests to /api/products
 
 app.listen(PORT, () => {
   connectDB(); // Connect to the database using the environment variable for the MongoDB URI
